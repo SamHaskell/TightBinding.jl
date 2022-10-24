532ac82feada0af421f1b2cf8888809c707290cb
SamHaskell <samuel.haskell@manchester.ac.uk>
2022-10-24 08:55:55 +0100

    24 Oct 2022

--- src/TightBinding.jl
+++ src/TightBinding.jl
@@ -2,8 +2,11 @@ module TightBinding
 
 using LinearAlgebra
 using Documenter
+using StaticArrays
 
-export dual, intralayerblock, intralayerhamiltonian, brillouinzone, occupancymatrix, meanfieldmatrix
+export  dual, intralayerblock, intralayerhamiltonian, brillouinzone, 
+        occupancymatrix, meanfieldmatrix, phasematrix, meanfieldpartial,
+        meanfielditerator, meanfieldupdate
 
 function dual(u1, u2)
     
@@ -57,32 +60,29 @@ function intralayerblock(K, J, Γ, uK, uJ, uΓ, uC, alpha)
     j = beta + 2
     k = gamma + 2
     
-    U = [0 0 0 0;
-         0 0 0 0;
-         0 0 0 0;
-         0 0 0 0]
+    U = zeros(Float64, 4, 4)
 
     U[1, 1] = (K+J)*uK + 2Γ*uΓ + 2J*uJ
     U[i, i] = (K+J)*uC
     U[j, j] = J*uC
     U[k, k] = J*uC
-    U[j, k] = Γ*uΓ
-    U[k, j] = Γ*uΓ
+    U[j, k] = Γ*uC
+    U[k, j] = Γ*uC
 
     return U
 end
 
-function intralayerhamiltonian(block_list, momentum, neighbour_list)
+function intralayerhamiltonian(block_list, momentum, nearest_neighbours)
     
     """
     Constructs the 8x8 k-dependant Hamiltonian block for a single KHG layer.
     """
     
-    H = Array{ComplexF64}(undef, 4, 4)
+    H = zeros(ComplexF64, 4, 4)
     Z = zeros(ComplexF64, 4, 4)
 
     for i in 1:3
-        H += 1im*block_list[i]*exp(1im*dot(momentum, neighbour_list[i]))
+        H += 1im*block_list[i]*exp(1im*dot(momentum, nearest_neighbours[i]))
     end
 
     return -0.25*vcat(hcat(Z, H), hcat(H', Z))
@@ -98,6 +98,18 @@ function occupancymatrix(values, temperature=0, mu=0)
     return diagm([1/(exp((E-mu)/temperature)+1) for E in values])
 end
 
+function phasematrix(momentum, translation)
+
+    """
+    Gives a diagonal 8x8 matrix U such that UAU^{†} applies the correct phase to the
+    off-diagonal blocks of the layer Hamiltonian.
+    """
+
+    phase = exp(0.5im*dot(momentum, translation))
+    phase_list = [phase, phase, phase, phase, phase', phase', phase', phase']
+    return diagm(phase_list)
+end
+
 function meanfieldmatrix(vectors, occupancy)
 
     """
@@ -106,6 +118,51 @@ function meanfieldmatrix(vectors, occupancy)
     """
 
     return vectors*(occupancy*vectors')
+end
+
+function meanfieldpartial(fields, phases)
+
+    """
+    Compute a matrix whose upper half triangle contains each of the k-momentum'th
+    partial summands of our mean-fields.
+    """
+
+    mat = 1im*phases'*(fields*phases)
+
+    return mat - transpose(mat)
+end
+
+function meanfielditerator(K, J, Γ, init_fields, brillouin_zone, nearest_neighbours, alpha, temperature = 0)
+    
+    inds = [(6, 2), (7, 3), (7, 2), (5, 1)]
+    uK, uJ, uΓ, uC = [init_fields[x, y] for (x, y) in inds]
+
+    block_list = [intralayerblock(K, J, Γ, uK, uJ, uΓ, uC, i-1) for i in 1:3]
+    N = 2*Int(length(brillouin_zone))
+    display(brillouin_zone)
+    println(N)
+    new_fields = zeros(ComplexF64, 8, 8)
+
+    for k in brillouin_zone
+        H = intralayerhamiltonian(block_list, k, nearest_neighbours)
+        vals, vecs = eigen(H)
+        occ = occupancymatrix(vals, temperature)
+        fields = meanfieldmatrix(vecs, occ)
+        phases = phasematrix(k, nearest_neighbours[alpha])
+        new_fields += meanfieldpartial(fields, phases)
+    end
+
+    return new_fields/N
+end
+
+function meanfieldupdate(init_fields, new_fields, mixing)
+
+    """
+    Uses successive over-relaxation/mixed updating to 
+    generate new sets of mean-fields for each iteration.
+    """
+
+    return (1-mixing)*init_fields + mixing*new_fields
 
 end
 
